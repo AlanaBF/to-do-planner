2b632860718d0dfec380d9452f69bcc0f0f0737e
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-05-15 22:37:59 +0100

    working on items adding and deleting on separate lists

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const date = require(__dirname + "/date.js");
 
 const app = express();
 
-const items = [];
-const workItems = [];
-const shoppingItems = [];
-const holidayItems = [];
+let items = [];
+let workItems = [];
+let shoppingItems = [];
+let holidayItems = [];
 
 app.set("view engine", "ejs");
 
@@ -29,27 +29,35 @@ app.get("/general", function (req, res) {
   //from date module. Could use date.getDay() as well
   const day = date.getDay() + "<br/>" + " General To Do List";
   //list links to the list.ejs template
-  res.render("list", { listTitle: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items, list: "general" });
 });
 
 app.post("/general", function (req, res) {
   const item = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
+  // if (req.body.list === "Work") {
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // } else {
     items.push(item);
     res.redirect("/general");
-  }
+  //}
 });
+
+app.delete("/general/:item", function(req, res) {
+  const {item} = req.params
+  items = items.filter(i => {
+    i !== item
+  })
+  res.status(200).json({message:"deleted"})
+})
 
 // ------------------------------------------------------------------ \\
 // Code for Work To Do List Page
 
 app.get("/work", function (req, res) {
   const day = date.getDay() + "<br/>" + "Work To Do List";
-  res.render("list", { listTitle: day, newListItems: workItems });
+  res.render("list", { listTitle: day, newListItems: workItems, list: "work" });
 });
 
 app.post("/work", function (req, res) {
@@ -58,12 +66,20 @@ app.post("/work", function (req, res) {
   res.redirect("/work");
 });
 
+app.delete("/work/:item", function(req, res) {
+  const {item} = req.params
+  items = items.filter(i => {
+    i !== item
+  })
+  res.status(200).json({message:"deleted"})
+})
+
 // ------------------------------------------------------------------ \\
 // Code for Shopping To Do List Page
 
 app.get("/shopping", function (req, res) {
   const day = date.getDay() + "<br/>" + "Shopping To Do List";
-  res.render("list", { listTitle: day, newListItems: shoppingItems });
+  res.render("list", { listTitle: day, newListItems: shoppingItems, list: "shopping" });
 });
 
 app.post("/shopping", function (req, res) {
@@ -72,12 +88,20 @@ app.post("/shopping", function (req, res) {
   res.redirect("/shopping");
 });
 
+app.delete("/shopping/:item", function(req, res) {
+  const {item} = req.params
+  items = items.filter(i => {
+    i !== item
+  })
+  res.status(200).json({message:"deleted"})
+})
+
 // ------------------------------------------------------------------ \\
 // Code for Holiday To Do List Page
 
 app.get("/holiday", function (req, res) {
   const day = date.getDay() + "<br/>" + "Holiday To Do List";
-  res.render("list", { listTitle: day, newListItems: holidayItems });
+  res.render("list", { listTitle: day, newListItems: holidayItems, list: "holiday"});
 });
 
 app.post("/holiday", function (req, res) {
@@ -85,6 +109,16 @@ app.post("/holiday", function (req, res) {
   holidayItems.push(item);
   res.redirect("/holiday");
 });
+
+app.delete("/holiday/:item", function(req, res) {
+  const {item} = req.params
+  items = items.filter(i => {
+    i !== item
+  })
+  res.status(200).json({message:"deleted"})
+})
+
+// ------------------------------------------------------------------ \\
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("server started on port 3000");
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,11 @@
 $(".deleteMe").on("click", function(){
    $(this).closest("div").remove(); 
+   const list = $(this).data("list")
+   const item = $(this).data("item")
+ fetch(`/${list}/${item}`, {method:"DELETE"})
+ .then(response => {
+    console.log(response)
+ }).catch(error => {
+    console.log(error)
+ })
  });
